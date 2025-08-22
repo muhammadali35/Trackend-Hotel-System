c4120ef4229170ed26406257e8bffb79b23eaff9
Muhammad Ali <muhammadalikatia123@gmail.com>
2025-08-22 03:14:23 -0700

    fix: removed unused import from Nav.jsx

--- Fronted/frontend/src/Tour/Home/Nav.jsx
+++ Fronted/frontend/src/Tour/Home/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { use, useContext, useState } from "react";
+import React, {  useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import logo from './../../assests/logo1.webp';
